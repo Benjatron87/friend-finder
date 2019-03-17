8dabe101a06b2bdbf24024227106abd2ddc714e1
Benjatron <benjt122@gmail.com>
2019-03-17 15:57:18 -0400

    App can push user infor to the api

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+const friends = [{
+        name: "Benj Thomsen",
+        photo: "../../myImage/my-image.jpg",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "4",
+            "3",
+            "4",
+            "2",
+            "0",
+            "4",
+            "4"
+        ]
+    },
+    {
+        name:"Horace Face",
+        photo:"https://ichef.bbci.co.uk/news/660/cpsprodpb/B875/production/_102512274_gettyimages-518360318.jpg",
+        scores:[
+            "4",
+            "0",
+            "4",
+            "4",
+            "3",
+            "1",
+            "2",
+            "2",
+            "4",
+            "0"
+        ]
+      }
+]
+
+module.exports = friends
