94a169708769eb592daee4005aa343b4717b890d
Benjatron <benjt122@gmail.com>
2019-03-18 17:42:41 -0400

    Added some more random users

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,6 +15,22 @@ const friends = [{
         ]
     },
     {
+        name: "Jonathan Flurry",
+        photo: "https://www.hbo.com/content/dam/hbodata/series/game-of-thrones/character/s5/john-snow-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg",
+        scores: [
+            "3",
+            "3",
+            "1",
+            "4",
+            "3",
+            "2",
+            "2",
+            "0",
+            "4",
+            "4"
+        ]
+    },
+    {
         name:"Horace Face",
         photo:"https://ichef.bbci.co.uk/news/660/cpsprodpb/B875/production/_102512274_gettyimages-518360318.jpg",
         scores:[
@@ -29,7 +45,39 @@ const friends = [{
             "4",
             "0"
         ]
-      }
+      },
+      {
+        name:"Huge Jacked Man",
+        photo:"https://is1-ssl.mzstatic.com/image/thumb/Video122/v4/df/c4/31/dfc4319a-6b5b-ef00-d493-f1163dd2c3b9/pr_source.lsr/268x0w.png",
+        scores:[
+            "1",
+            "0",
+            "2",
+            "3",
+            "1",
+            "0",
+            "3",
+            "4",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Ran Dome",
+        photo: "https://images-na.ssl-images-amazon.com/images/I/512%2BMrmB8GL._SY355_.jpg",
+        scores: [
+            "0",
+            "2",
+            "1",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "2",
+            "2"
+        ]
+    }
 ]
 
 module.exports = friends
